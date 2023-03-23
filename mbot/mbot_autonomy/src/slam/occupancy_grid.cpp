72368b6ea529f58b9807445849d6178f36790249
Sam Parham <sparham@umich.edu>
2023-03-23 12:25:41 -0400

    working score ray, score endpoint for  --mapping-only

--- mbot/mbot_autonomy/src/slam/occupancy_grid.cpp
+++ mbot/mbot_autonomy/src/slam/occupancy_grid.cpp
@@ -83,17 +83,37 @@ void OccupancyGrid::addLogOdds(int x, int y, CellOdds value)
     {
         CellOdds upperLimit = numeric_limits<CellOdds>::max();
         CellOdds lowerLimit = numeric_limits<CellOdds>::min();
-        // If adding value increases (x, y) past CellOdds upperLimit, set it to the upperLimit
-        if (upperLimit - operator()(x, y) < value) {
-            operator()(x, y) = upperLimit;
+        // if positive
+        if (value > 0) {
+            if (upperLimit - operator()(x, y) < value) {
+                operator()(x, y) = upperLimit;
+            }
+            else {
+                operator()(x, y) += value;
+            }
         }
-        // Otherwise, if adding value decreases (x, y) past CellOdds lowerLimit, set it to the lowerLimit
-        else if (lowerLimit - operator()(x, y) < value) {
-            operator()(x, y) = lowerLimit;
-        }
+        // if negative
         else {
-            operator()(x, y) += value;
+            if (lowerLimit - operator()(x, y) > value) {
+                operator()(x, y) = lowerLimit;
+            }
+            else {
+                operator()(x, y) += value;
+            }
         }
+
+
+        // // If adding value increases (x, y) past CellOdds upperLimit, set it to the upperLimit
+        // if (upperLimit - operator()(x, y) < value) {
+        //     operator()(x, y) = upperLimit;
+        // }
+        // // Otherwise, if adding value decreases (x, y) past CellOdds lowerLimit, set it to the lowerLimit
+        // else if (lowerLimit - operator()(x, y) < value) {
+        //     operator()(x, y) = lowerLimit;
+        // }
+        // else {
+        //     operator()(x, y) += value;
+        // }
     }
 }
 
